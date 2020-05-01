3c822ab7d8f7d0729e9a86c6b502fe53403efc52
Murshed J. Ahmed <mahmed@marvel.com>
2020-04-30 21:57:47 -0400

    clean up output, add flag to shuffle questions

--- main.go
+++ main.go
@@ -7,21 +7,26 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"os"
 	"strings"
 	"time"
 )
 
 func main() {
+	// Define command line flags
 	pFilename := flag.String("file", "problems.csv", "The name of the CSV file that contains the questions and answers")
 	pTimeout := flag.Int("time", 30, "Amount of time to finish the quiz")
+	pShuffle := flag.Bool("shuffle", false, "Randomize the questions read from the CSV file")
 	flag.Parse()
 
 	seconds := (*pTimeout)
+
+	// Channel that will be written to if the user finishes on time
 	ch := make(chan int)
 	defer close(ch)
 
-	fmt.Println("Quiz will be from", *pFilename)
+	fmt.Println("This quiz will be from", *pFilename)
 
 	csvFile, err := os.Open(*pFilename)
 	if err != nil {
@@ -33,27 +38,56 @@ func main() {
 	points := 0
 	questionsAnswered := 0
 
-	fmt.Println(seconds, "seconds on the clock")
-	fmt.Print("Press <Enter> when you are ready")
+	var lines [][]string
+	for {
+		record, err := reader.Read()
+
+		// Break out of the loop once we've reached the end of the file
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if len(record) != 2 {
+			continue
+		}
+
+		// Clean up the answer to give the user a better chance at
+		// correctly answering it
+		answer := strings.ToLower(record[1])
+		answer = strings.TrimSpace(answer)
+		record[1] = answer
+
+		lines = append(lines, record)
+	}
+	totalQuestions := len(lines)
+
+	if totalQuestions == 0 {
+		fmt.Println("There are no valid questions in", *pFilename)
+		os.Exit(1)
+	}
+
+	// Randomize the questions if desired
+	if *pShuffle {
+		rand.Seed(time.Now().UnixNano())
+		rand.Shuffle(len(lines), func(i, j int) {
+			lines[i], lines[j] = lines[j], lines[i]
+		})
+	}
+
+	fmt.Println(seconds, "seconds on the clock.")
+	fmt.Println("There are", totalQuestions, "questions in total.")
+	fmt.Print("Press <Enter> when you are ready ... ")
 	scanner.Scan()
 	fmt.Println("Begin!")
 	fmt.Println()
 
 	go func() {
-		for {
-			record, err := reader.Read()
-			if err == io.EOF {
-				break
-			}
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			question := record[0]
-			answer := strings.ToLower(record[1])
-			answer = strings.TrimSpace(answer)
-
+		for _, line := range lines {
+			question, answer := line[0], line[1]
 			fmt.Print(question, " = ")
 			scanner.Scan()
 			userInput := scanner.Text()
@@ -73,11 +107,15 @@ func main() {
 
 	select {
 	case <-ch:
-		fmt.Println("You got", points, "out of", questionsAnswered)
+		fmt.Println()
+		fmt.Println("You finished!")
 	case <-time.After(time.Duration(seconds) * time.Second):
 		fmt.Println()
 		fmt.Println("You ran out of time!")
-		fmt.Println("You got", points, "out of", questionsAnswered)
 	}
 
+	fmt.Println("Questions Answered:", questionsAnswered, "of", totalQuestions)
+	fmt.Println("Correct Answers:", points)
+	percent := 100 * float32(points) / float32(totalQuestions)
+	fmt.Printf("Score: %.1f %%", percent)
 }
